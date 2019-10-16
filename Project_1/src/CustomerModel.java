e0c841fcfc09d44c410348b6a61c2b7031f76f1a
Theo Zinner <tvz6123@gmail.com>
2019-10-15 19:50:15 -0500

    Project_1 COMP3700
    This java program interacts with a sql database to keep track of customers, products and purchases.

---
+++ Project_1/src/CustomerModel.java
@@ -0,0 +1,14 @@
+
+public class CustomerModel {
+    public int mCustomerID;
+    public String mName, mAddress, mPhone;
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder("(");
+        sb.append(mCustomerID).append(",");
+        sb.append("\"").append(mName).append("\"").append(",");
+        sb.append("\"").append(mAddress).append("\"").append(",");
+        sb.append(mPhone).append(")");
+        return sb.toString();
+    }
+}
